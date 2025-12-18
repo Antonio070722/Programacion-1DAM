c4a0fab2684493d8e8dd16ebd7851a7a83d1edd6
PC-CLASE <allopal2911@g.educaand.es>
2025-12-18 16:49:32 +0100

    añadidos los usuarios y sus contraseñas en el hashmap, y por hacer el metodo de pedir los datos al usuario

--- Ejercicios/src/primer_trimestre/Tema10/RECAP1EVAL/Principal.java
+++ Ejercicios/src/primer_trimestre/Tema10/RECAP1EVAL/Principal.java
@@ -5,6 +5,18 @@ import java.util.HashMap;
 public class Principal {
     static void main(String[] args) {
         HashMap<String, Long> usuario = new HashMap<>();
+        usuario.put("admin", 1234L);
+        usuario.put("alumno", 1111L);
+        usuario.put("profesor", 2222L);
+
+        pedirdatos();
 
     }
+
+    public static String pedirdatos(){
+
+
+
+    }
+
 }
