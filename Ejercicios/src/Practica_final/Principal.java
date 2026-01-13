1b37b0f606b258c7d2027c36b01f373764ec7d89
PC-CLASE <allopal2911@g.educaand.es>
2026-01-13 20:43:11 +0100

    Creado el paquete para la práctica final

--- Ejercicios/src/Practica_final/Principal.java
+++ Ejercicios/src/Practica_final/Principal.java
@@ -1,5 +1,26 @@
 package Practica_final;
 
+import java.util.HashMap;
+import java.util.Scanner;
+
 public class Principal {
+    static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String usuario = "";
+        String password = "";
+        do {
+            System.out.println("Bienvenido! Login:");
+            System.out.println("Introduce tu usuario: ");
+            usuario = sc.nextLine();
+            System.out.println("Introduce tu contraseña: ");
+            password = sc.nextLine();
+        }while(!loginCorrecto(usuario, password));
+    }
+
+    public static boolean loginCorrecto(String usuario, String password) {
+        HashMap<String, String> usuarios = new HashMap<>();
+        usuarios.put("admin", "contraseñaValida");
+        usuarios.put("alumno", "contraValida");
+    }
 
 }
